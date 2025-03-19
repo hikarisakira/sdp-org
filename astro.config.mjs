1c3be2d819747af62f92aa6fdb70d2f6ad2b6768
Cheng-Yang, Li <hsakira.k861@mmh.org.tw>
2025-03-19 23:19:21 +0800

    init

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import react from '@astrojs/react';
+
+import svelte from '@astrojs/svelte';
+
+import vercel from '@astrojs/vercel';
+
+import mdx from '@astrojs/mdx';
+
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), svelte(), mdx()],
+  adapter: vercel(),
+  vite:{
+    // @ts-ignore
+    plugins: [tailwindcss()],
+  }
+});
