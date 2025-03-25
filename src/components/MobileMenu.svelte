3b64ed2a303ba4eef4c6067529de1690704efe9a
Cheng-Yang, Li <hsakira.k861@mmh.org.tw>
2025-03-25 22:17:44 +0800

    fix logo

--- src/components/MobileMenu.svelte
+++ src/components/MobileMenu.svelte
@@ -17,7 +17,7 @@
 </script>
 
 <button class="btn btn-square btn-ghost" on:click={toggleMenu}>
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
 </button>
 
 {#if isMenuOpen}
@@ -25,7 +25,6 @@
   <div class="fixed inset-y-0 left-0 w-64 bg-base-100 z-50" transition:slide={{ duration: 300 }}>
     <ul class="menu p-4 w-full">
       {#if !currentSubmenu}
-        <li><a href="/" on:click={toggleMenu}>首頁</a></li>
         <li><button on:click={() => openSubmenu('news')}>最新消息</button></li>
         <li><button on:click={() => openSubmenu('about')}>關於社民黨</button></li>
         <li><a href="/join" class="btn btn-primary mt-4">加入我們</a></li>
