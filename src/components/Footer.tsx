e50ebc819658f96d9465340ab7f12546dca01754
Cheng-Yang, Li <hsakira.k861@mmh.org.tw>
2025-08-28 17:08:55 +0800

    Add carousel base

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,6 @@ import ContactButton from './footer/contact';
 export default function Footer() {
     return(
     <>
-        <link rel="stylesheet" href="./node_modules/lineicons/dist/lineicons.css" />
         <footer className="footer lg:footer-horizontal text-white p-10" id='footer-color'>
             <LogoZone />
             
