1aa499a986933e662fbd2df01b0369212c06bc7a
Cheng-Yang, Li <hsakira.k861@mmh.org.tw>
2025-12-08 15:29:23 +0800

    add news contents

--- tina/collections/blog.ts
+++ tina/collections/blog.ts
@@ -1,7 +1,6 @@
 import type { Collection } from "tinacms";
 
 export const BlogCollection: Collection = {
-
   name: "blog",
   label: "Blogs",
   path: "src/content/blog",
@@ -15,35 +14,51 @@ export const BlogCollection: Collection = {
     {
       type: "string",
       name: "title",
-      label: "Title",
+      label: "文章標題",
       isTitle: true,
       required: true,
     },
     {
       name: "description",
-      label: "Description",
+      label: "文章簡述",
       type: "string",
     },
     {
       name: "pubDate",
-      label: "Publication Date",
+      label: "發布日期",
       type: "datetime",
     },
     {
       name: "updatedDate",
-      label: "Updated Date",
+      label: "更新時間",
       type: "datetime",
     },
     {
       name: "heroImage",
-      label: "Hero Image",
+      label: "標題圖片",
       type: "image",
+    },
+    {
+      name: "tags",
+      label: "文章標籤分類",
+      type: "string",
+      list: true,
+      ui: {
+        component: "tags",
+        description: "在此處添加標籤以分類文章類別",
+      },
+      options: [
+        { value: "press", label: "新聞稿" },
+        { value: "events", label: "活動資訊" },
+        { value: "issues", label: "議題發言" },
+        { value: "clarification", label: "闢謠專區" },
+      ],
     },
     {
       type: "rich-text",
       name: "body",
-      label: "Body",
+      label: "文章內容",
       isBody: true,
     },
   ],
-}
+};
